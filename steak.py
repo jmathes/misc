e0997aab34320c0ad13858de20d52ae2764d3179
Joseph Mathes <jmathes@gmail.com>
2018-02-10 18:33:30 -0800

    basic steak

---
+++ steak.py
@@ -0,0 +1,15 @@
+import time
+import os
+
+
+while True:
+  time.sleep(8)
+  for i in xrange(60):
+    print 60 - i
+    time.sleep(1)
+  os.system("say one minute is up")
+  time.sleep(8)
+  for i in xrange(60):
+    print 60 - i
+    time.sleep(1)
+  os.system("say one minute is up again")  
